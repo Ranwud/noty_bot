2af32356683022a0cc769423cdfcf3bb9182763e
unknown <vovan.hohlov@gmail.com>
2023-05-28 18:39:07 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# noty_bot
-Telegram bot for regular scheduled notifications
+# Notify bot
+This is a Telegram bot that sends regular notifications to a chat. You can customize the notifications by editing the config.yaml file.
+
+## Installation
+
+To use this bot, please follow the instructions below:
+
+1. Clone this repository to your local machine and go to the project folder:
+```shell
+git clone https://github.com/Ranwud/noty_bot.git
+```
+
+```shell
+cd noty_bot
+```
+
+2. Make sure you have Python 3.x installed.
+3. Install the required libraries by running the following command:
+
+```shell
+pip install -r requirements.txt
+```
+
+4. Create a new Telegram bot and obtain the bot token. You can create a new bot by talking to @BotFather on Telegram and following the instructions.
+5. Open the config.yaml file and edit it according to your notification requirements. Please make sure to strictly adhere to the specified format and indentation.
+6. To start the Telegram notification bot, run the following command in the terminal:
+
+```shell
+python main.py
+```
+or comand
+
+```shell
+python3 main.py
+```
+
+The bot will start reading the config.yaml file and sending notifications based on the specified configurations.
+
+Make sure to keep the bot running in the background or on a server to ensure continuous notifications.
+
+## Installation
+
+The required libraries for running this bot are listed in the `requirements.txt` file. You can install them using the following command:
+
+```shell
+pip install -r requirements.txt
+```
+
+Please note that it's recommended to create a virtual environment before installing the requirements to keep your Python environment clean.
+
+Now you are ready to set up and use the Telegram notification bot. Customize the `config.yaml` file according to your notification needs, start the bot, and enjoy regular notifications in your specified Telegram chat!
---
+++ main.py
@@ -0,0 +1,124 @@
+import yaml
+import telebot
+import datetime
+import time
+import logging
+
+# Инициализация логгера
+logger = logging.getLogger('bot')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# Добавление обработчика для записи в файл
+file_handler = logging.FileHandler('bot.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Добавление обработчика для вывода в консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+# Загрузка параметров из файла config.yaml
+def load_config():
+    try:
+        with open("config.yaml", "r") as file:
+            config = yaml.safe_load(file)
+        return config
+    except Exception as e:
+        logger.error(f"Failed to load config.yaml. Error: {str(e)}")
+        return None
+
+# Перезагрузка параметров из файла config.yaml
+def reload_config(config):
+    new_config = load_config()
+    if new_config:
+        config.update(new_config)
+
+# Отправка уведомления
+def send_notification(bot, chat_id, text):
+    try:
+        bot.send_message(chat_id, text, parse_mode='HTML')
+        logger.info(f"Sent notification to chat_id {chat_id}: {text}")
+    except Exception as e:
+        logger.error(f"Failed to send notification to chat_id {chat_id}: {text}. Error: {str(e)}")
+
+# Проверка, должно ли быть отправлено уведомление в данный момент
+def should_send_notification(notification):
+    now = datetime.datetime.now()
+    start_time = [datetime.datetime.strptime(t, '%H:%M:%S').time() for t in notification['start_time']]
+    end_time = [datetime.datetime.strptime(t, '%H:%M:%S').time() for t in notification['end_time']]
+    interval = notification['interval']
+    period = notification['period']
+    day_of_week = notification.get('day_of_week', None)
+    days_of_week = notification.get('days_of_week', None)
+
+    if period == 'daily':
+        for start, end in zip(start_time, end_time):
+            if start <= now.time() <= end and (now.minute % interval == 0):
+                return True
+    elif period == 'weekday':
+        if now.weekday() < 5:
+            for start, end in zip(start_time, end_time):
+                if start <= now.time() <= end and (now.minute % interval == 0):
+                    return True
+    elif period == 'weekend':
+        if now.weekday() >= 5:
+            for start, end in zip(start_time, end_time):
+                if start <= now.time() <= end and (now.minute % interval == 0):
+                    return True
+    elif period in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+        if now.weekday() == ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].index(period):
+            for start, end in zip(start_time, end_time):
+                if start <= now.time() <= end and (now.minute % interval == 0):
+                    return True
+    elif period == 'weekly' and day_of_week is not None:
+        if now.weekday() == day_of_week:
+            for start, end in zip(start_time, end_time):
+                if start <= now.time() <= end and (now.minute % interval == 0):
+                    return True
+    elif period == 'weekday' and days_of_week is not None:
+        if now.weekday() in days_of_week:
+            for start, end in zip(start_time, end_time):
+                if start <= now.time() <= end and (now.minute % interval == 0):
+                    return True
+
+    return False
+
+# Обработка ошибок при отправке уведомления
+def handle_send_notification_error(bot, chat_id, text, error):
+    logger.error(f"Failed to send notification to chat_id {chat_id}: {text}. Error: {str(error)}")
+    # Дополнительная логика обработки ошибки, например, отправка уведомления администратору
+
+# Основная функция бота
+def main():
+    # Загрузка параметров из файла config.yaml
+    config = load_config()
+
+    if not config:
+        logger.error("Failed to start the bot. Config not loaded.")
+        return
+
+    # Создание экземпляра бота
+    bot = telebot.TeleBot(config['bot_token'])
+
+    # Бесконечный цикл для проверки и отправки уведомлений
+    while True:
+        try:
+            reload_config(config)  # Перезагрузка параметров из файла config.yaml
+            for notification in config['notifications']:
+                if should_send_notification(notification):
+                    chat_id = notification['chat_id']
+                    text = notification['text']
+                    try:
+                        send_notification(bot, chat_id, text)
+                    except Exception as e:
+                        handle_send_notification_error(bot, chat_id, text, e)
+            time.sleep(60)  # Задержка в 1 минуту перед следующей проверкой
+        except Exception as e:
+            logger.error(f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    main()
